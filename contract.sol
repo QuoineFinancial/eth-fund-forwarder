12ba7ed19b9e48a8d7cc74d6514f004acb3b155f
Chinh Nguyen <chinh7@me.com>
2018-06-15 01:25:55 +0700

    apply common interfaces to existing contracts

--- contract.sol
+++ contract.sol
@@ -90,32 +90,42 @@ contract ContractToken {
     function balanceOf(address user) public returns (uint256);
 }
 
-// Contract used for forwarding ETH and token from UserWallet to exchange wallet
-contract FundForwardContract {
+/**
+ * @title FundForwardContract used for forwarding ETH and token from UserWallet to exchange wallet
+ */
+contract FundForwardContract {    
     Wallet wallet;
+    event FundForwarded(address contractToken, address from, address to, uint amount);
     
     constructor(address _wallet) public {
         wallet = Wallet(_wallet);
     }
     
+    /**
+     * @dev Throws if called by any account other than fund forwarder.
+     */
     modifier onlyFundForwarder() {
         require(msg.sender == wallet.fundForwarder());
         _;
     }
-    
+    /**
+     * @dev Throws if wallet is not running.
+     */    
     modifier walletRunning() {
-        require(wallet.running() == true);
+        require(wallet.paused() == false);
         _;
     }
-
-    // only forwarder from wallet has the right to call this function
+    /**
+     * @notice Forward assets from userWallets to fundDestination
+     * @dev only forwarder from wallet has the right to call this function
+     */
     function forward(address _id)
     onlyFundForwarder
     walletRunning
     public 
     returns (bool success) {
         success = false;
-        address destination = wallet.mainWallet();
+        address destination = wallet.fundDestination();
         uint amount;
 
         // default address 0x000...00 for transfer ETH
@@ -127,11 +137,11 @@ contract FundForwardContract {
             success = token.transfer(destination, amount);
         } else {
             amount = address(this).balance;
-            success = destination.send(amount);
+            success = destination.transfer(amount);
         }
         
         if (success) {
-            wallet.logForward(_id, this, destination, amount);
+            emit FundForwarded(_id, this, destination, amount);
         }
     }
 }
@@ -158,7 +168,7 @@ contract UserWallet {
         (_from);
         (_value);
         (_data);
-     }
+    }
 
     // only fundforwarder has the right to call forward
     function forward(address _id) 
@@ -170,30 +180,20 @@ contract UserWallet {
     }
 }
 
-contract Owner {
+
+contract Owner is Pausable{
     address public mainWallet; // default address keep ETH and token
-    address public owner; // owner has the right to change mainWallet, and fundforwarder
     address public fundForwarder; // default address has the right to call forward on UserWallet
     
     constructor(address _fundForwarder) public {
-        owner = msg.sender;
         mainWallet = msg.sender;
         fundForwarder = _fundForwarder;
-    }
-    
-    modifier onlyOwner() {
-        require(msg.sender == owner);
-        _;
     }
     
     function changeMainWallet(address _mainWallet) onlyOwner public {
         mainWallet = _mainWallet;
     }
     
-    function changeOwner(address _owner) onlyOwner public {
-        owner = _owner;
-    }
-
     function changeFundForwarder(address _fundForwarder) onlyOwner public {
         fundForwarder = _fundForwarder;
     }
@@ -201,24 +201,13 @@ contract Owner {
 
 // Generate new address for user
 // The new address can be used for ETH and other ERC20 Token
-contract Generator {
-    address public owner; // default address has the right to make new UserWallet contract
-    
+contract Generator is Ownable{
     event LogAddress(address _address);
-    
-    constructor() public {
-        owner = msg.sender;
-    }
-    
-    modifier onlyOwner() {
-        require(msg.sender == owner);
-        _;
-    } 
     
     // new wallet is created only if it was called by the owner,
     function generate(address vendor) 
-        onlyOwner
-        public
+    onlyOwner
+    public
     returns (address newAddress) {
         newAddress = address(new UserWallet(vendor));
         emit LogAddress(newAddress);
@@ -236,27 +225,13 @@ contract Wallet is Owner {
 
     constructor(address _fundForwarder) Owner(_fundForwarder) { }
     
-    event LogForward(address contractToken, address from, address to, uint amount);
-    
     function updateForwardContract(address _token, address _address) onlyOwner public {
         forwardContract[_token] = _address;
-    }
-    
-    function resume() onlyOwner public {
-        running = false;
-    }
-    
-    function pause() onlyOwner public {
-        running = true;
     }
     
     // by default, every tokens and ETH will be forward by defaultForwarder
     function getForwardContract(address _token) public returns (address res) {
         res = forwardContract[_token];
         if (res == 0) res = defaultForwardContract;
-    }
-    
-    function logForward(address token, address from, address to, uint amount) public {
-        emit LogForward(token, from, to, amount);
     }
 }
